c204e54310847e5ab4a7c5ff50140559b30d0725
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-11-30 11:41:30 +0000

    Initial rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueCarbon::Application.config.secret_token = '81ec7ce87fa66ff7e136bbb56c10b000ada1ad8ae7faa1f157e5e502bbf6deb2a13e0774cd1fcd1de809004332ae147e0ae3ff6ed35dcb54f46f90949c43c5ae'
