322a8cfc8dca5922cdfb7a430ea92ca931ca10c7
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-12-04 09:15:21 +0000

    Add SQLite3 to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,3 +33,35 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+# SQLite3 configuration
+
+set(:shared_database_path) {"#{shared_path}/databases"}
+
+namespace :sqlite3 do
+  desc "Make a shared database folder"
+  task :make_shared_folder, :roles => :db do
+    run "mkdir -p #{shared_database_path}"
+  end
+
+  desc "Generate a database configuration file"
+  task :build_configuration, :roles => :db do
+    db_options = {
+      "adapter"  => "sqlite3",
+      "database" => "#{shared_database_path}/production.sqlite3"
+    }
+    config_options = {"production" => db_options}.to_yaml
+    run "mkdir -p #{shared_path}/config"
+    put config_options, "#{shared_path}/config/sqlite_config.yml"
+  end
+
+  desc "Links the configuration file"
+  task :link_configuration_file, :roles => :db do
+    run "ln -nsf #{shared_path}/config/sqlite_config.yml #{latest_release}/config/database.yml"
+  end
+end
+
+after "deploy:setup", "sqlite3:make_shared_folder"
+after "deploy:setup", "sqlite3:build_configuration"
+
+after "deploy:update_code", "sqlite3:link_configuration_file"
