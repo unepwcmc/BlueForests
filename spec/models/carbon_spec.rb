d3584918cd8f67f28b4a0c1970f3a78fe800a56b
migtorres <miguel.torres@unep-wcmc.org>
2013-04-03 13:46:20 +0100

    added carbon queries and tests

---
+++ spec/models/carbon_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+require_relative '../../lib/carbon_query'
+
+describe CarbonQuery do
+  describe '.query' do
+    before(:all) do
+      add_spatial_ref_sys = system("psql -U postgres -d bluecarbon_test -a -f /home/miguelt/postgis/postgis-2.0.2SVN/extensions/postgis/sql_bits/spatial_ref_sys.sql")
+    end
+    describe 'when there is one geometry that intersect' do
+      before(:each) do
+         puts add_query = "INSERT INTO carbon_view (c_mg_ha, area_ha, habitat, the_geom) VALUES (1, 
+          ST_Area(ST_Transform(ST_SetSRID(ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))'), 4326),27040))/10000, 
+          'Algalmat',
+          ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))', 4326));"
+        @query1 = "SELECT c_mg_ha*area_ha as carbon FROM carbon_view"
+        @query2 = CarbonQuery.total("ST_GeomFromText('MultiPolygon(((1 1, 4 1, 4 4, 1 4, 1 1)))',4326)", "carbon_view")
+        ActiveRecord::Base.connection.execute(add_query)
+        ActiveRecord::Base.connection.execute(@query2)
+      end
+
+      it 'calculates a carbon value that is less than existing' do
+        check =  ActiveRecord::Base.connection.select_one(@query2)
+        result = ActiveRecord::Base.connection.select_one(@query1)
+        Float(check["carbon"]).should < Float(result["carbon"])
+      end
+  end
+  describe 'when there is one geometry that does not intersect' do
+      before(:each) do
+        #add_spatial_ref_sys = system("psql -U postgres -d bluecarbon_test -a -f /home/miguelt/postgis/postgis-2.0.2SVN/extensions/postgis/sql_bits/spatial_ref_sys.sql")
+        add_query = "INSERT INTO carbon_view (c_mg_ha, area_ha, habitat, the_geom) VALUES (1, 
+         ST_Area(ST_Transform(ST_SetSRID(ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))'), 4326),27040))/10000,
+          'Algalmat',
+          ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))', 4326));"
+        @query2 = CarbonQuery.total("ST_GeomFromText('MultiPolygon(((3 3, 4 3, 4 4, 3 4, 3 3)))',4326)", "carbon_view")
+        ActiveRecord::Base.connection.execute(add_query)
+        ActiveRecord::Base.connection.execute(@query2)
+      end
+
+      it 'calculates a null carbon value' do
+        check =  ActiveRecord::Base.connection.select_one(@query2)
+          check["carbon"] = nil
+      end
+  end
+  describe 'when there is a geometry that completely overlaps' do
+      before(:each) do
+        #add_spatial_ref_sys = system("psql -U postgres -d bluecarbon_test -a -f /home/miguelt/postgis/postgis-2.0.2SVN/extensions/postgis/sql_bits/spatial_ref_sys.sql")
+        add_query = "INSERT INTO carbon_view (c_mg_ha, area_ha, habitat, the_geom) VALUES (3, 
+         ST_Area(ST_Transform(ST_SetSRID(ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))'), 4326),27040))/10000,
+          'Algalmat',
+          ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))', 4326));"
+        @query2 = CarbonQuery.total("ST_GeomFromText('MultiPolygon(((-1 -1, 4 -1, 4 4, -1 4, -1 -1)))',4326)", "carbon_view")
+        ActiveRecord::Base.connection.execute(add_query)
+        ActiveRecord::Base.connection.execute(@query2)
+        @query1 = "SELECT c_mg_ha*area_ha as carbon FROM carbon_view"
+      end
+
+      it 'calculates a carbon value that equals existing' do
+        check =  ActiveRecord::Base.connection.select_one(@query2)
+        result = ActiveRecord::Base.connection.select_one(@query1)
+        Float(check["carbon"]) == Float(result["carbon"])
+      end
+    end
+
+    describe 'when there is a geometry that intersects 2 geometries' do
+      before(:each) do
+        #add_spatial_ref_sys = system("psql -U postgres -d bluecarbon_test -a -f /home/miguelt/postgis/postgis-2.0.2SVN/extensions/postgis/sql_bits/spatial_ref_sys.sql")
+        add_query = "INSERT INTO carbon_view (c_mg_ha, area_ha, habitat, the_geom) VALUES (3, 
+         ST_Area(ST_Transform(ST_SetSRID(ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))'), 4326),27040))/10000,
+          'Algalmat',
+          ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))', 4326));"
+        add_query2 = "INSERT INTO carbon_view (c_mg_ha, area_ha, habitat, the_geom) VALUES (3, 
+         ST_Area(ST_Transform(ST_SetSRID(ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))'), 4326),27040))/10000,
+          'Algalmat',
+          ST_GeomFromText('MULTIPOLYGON(((0 0, 2 0, 2 2, 0 2, 0 0)))', 4326));"
+
+        @query2 = CarbonQuery.total("ST_GeomFromText('MultiPolygon(((-1 -1, 4 -1, 4 4, -1 4, -1 -1)))',4326)", "carbon_view")
+        ActiveRecord::Base.connection.execute(add_query)
+        ActiveRecord::Base.connection.execute(@query2)
+        @query1 = "SELECT c_mg_ha*area_ha as carbon FROM carbon_view"
+      end
+
+      it 'calculates a carbon value that equals existing' do
+        check =  ActiveRecord::Base.connection.select_one(@query2)
+        result = ActiveRecord::Base.connection.select_one(@query1)
+        Float(check["carbon"]) == Float(result["carbon"])
+      end
+    end
+
+  end
+end
