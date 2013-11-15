98525da01ba373d9b89497a0492fac37c42f989a
Adam Mulligan <adammulligan@gmail.com>
2013-11-15 10:33:44 +0000

    Redirect to admin after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
     @current_ability ||= Ability.new(current_admin)
   end
 
+  def after_sign_in_path_for(resource)
+    validations_path
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access denied!"
     redirect_to root_url
