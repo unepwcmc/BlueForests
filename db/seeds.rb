3e240d2db3a1ebd9d1c610bc5b8ca03cea3e889f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-10 14:00:02 +0000

    remove unneded lines in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 role_admin = Role.create(name: 'admin')
 role_project_manager = Role.create(name: 'project_manager')
 role_project_participant = Role.create(name: 'project_participant')
@@ -15,8 +7,3 @@ admin = Admin.create(email: 'decio.ferreira@unep-wcmc.org', password: 'decioferr
 Role.update(1, description:'Can edit users, edit validations and create field sites')
 Role.update(2, description:'Can edit Validations and create field sites')
 Role.update(3, description:'Can make validations via the web or mobile tool')
-
-
-
-
-
