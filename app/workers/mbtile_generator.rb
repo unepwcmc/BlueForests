d715e948913506fac57d9e0a40db4dd416600c8b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-02-16 12:40:02 +0000

    embellish mbtile_generator with beautiful yield

--- app/workers/mbtile_generator.rb
+++ app/workers/mbtile_generator.rb
@@ -5,5 +5,13 @@ class MbtileGenerator
   def perform mbtile_id
     @mbtile = Mbtile.find(mbtile_id)
     return if @mbtile.already_generated?
+
+    while_generating { Mbtile::Project.generate(@mbtile.habitat, @mbtile.area) }
+  end
+
+  def while_generating
+    @mbtile.update_attributes(status: 'generating', last_generation_started_at: Time.now)
+    yield
+    @mbtile.update_attributes(status: 'complete', last_generated_at: Time.now)
   end
 end
