9e8cdcc63603c62746b66dbdf733f19406f738d5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-03-16 11:32:45 +0000

    rename Admin model to User

--- db/migrate/20121210105313_create_assignments.rb
+++ db/migrate/20121210105313_create_assignments.rb
@@ -1,7 +1,7 @@
 class CreateAssignments < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
-      t.integer :admin_id
+      t.integer :user_id
       t.integer :role_id
 
       t.timestamps
--- db/migrate/20150210155914_add_country_id_to_admin.rb
+++
@@ -1,5 +0,0 @@
-class AddCountryIdToAdmin < ActiveRecord::Migration
-  def change
-    add_column :admins, :country_id, :integer
-  end
-end
