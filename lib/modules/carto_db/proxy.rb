835e7c99911626d515849433d8342dbeae41e65e
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-04-03 16:06:53 +0100

    add new_map method to CartoDb::Proxy

--- lib/modules/carto_db/proxy.rb
+++ lib/modules/carto_db/proxy.rb
@@ -7,11 +7,40 @@ module CartoDb::Proxy
     CartoDb.get(url).body
   end
 
+  def self.new_map habitat, opts={}
+    sql = sql(habitat, opts[:country], opts[:where])
+    cartocss = opts[:style] if opts[:style]
+
+    carto_response = CartoDb.post(maps_url, body: mapconfig_json(sql, cartocss))
+    extract_tiles_url(carto_response.body)
+  end
+
   private
+
+  def self.extract_tiles_url response
+    info = JSON.parse(response)
+    tiles_path = URI.escape("/api/v1/map/#{info["layergroupid"]}/{z}/{x}/{y}.png")
+
+    CartoDb.build_url(tiles_path, with_api_key: false)
+  end
+
+  def self.mapconfig_json sql, cartocss
+    mapconfig_template.result(binding).squish
+  end
+
+  def self.mapconfig_template
+    @@mapconfig_template = ERB.new(
+      File.read(CartoDb::TEMPLATES_PATH.join('mapconfig.json.erb'))
+    )
+  end
+
+  def self.maps_url
+    CartoDb.build_url("/api/v1/map")
+  end
 
   def self.tiles_url table, coords, opts
     coords = "#{coords[:z]}/#{coords[:x]}/#{coords[:y]}"
-    CartoDb.build_url("/tiles/#{table}/#{coords}.png", opts)
+    CartoDb.build_url("/tiles/#{table}/#{coords}.png", query: opts)
   end
 
   def self.sql habitat, country, where
