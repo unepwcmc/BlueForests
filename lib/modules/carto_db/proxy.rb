40e9706d340a4bafeaa40f216c4c7a8aaee9c97a
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-04-14 15:35:59 +0100

    add CartoDb::Proxy::download_shapefile

--- lib/modules/carto_db/proxy.rb
+++ lib/modules/carto_db/proxy.rb
@@ -2,4 +2,8 @@ module CartoDb::Proxy
   def self.new_map habitat, opts={}
     CartoDb::Proxy::Map.create habitat, opts
   end
+
+  def self.download_shapefile habitat, opts={}
+    CartoDb::Proxy::Shapefile.download habitat, opts
+  end
 end
