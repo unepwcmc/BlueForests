eb29e0fccf95b3f5190cfcd77e674089233f2a43
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-07-13 18:53:31 +0100

    fix delete query and add some explanatory comments

--- lib/cartodb_query.rb
+++ lib/cartodb_query.rb
@@ -1,11 +1,16 @@
 class CartodbQuery
   def self.remove(table_name)
-
     <<-SQL
-    UPDATE #{table_name} SET toggle = true FROM
-(SELECT prev_phase, the_geom from #{table_name} g inner join (SELECT MAX(phase) as max_phase from #{table_name} g) a on g.phase = a.max_phase) a
-        WHERE #{table_name}.phase = a.prev_phase and #{table_name}.toggle = false AND ST_Intersects(#{table_name}.the_geom, a.the_geom);
-      DELETE FROM #{table_name} WHERE phase = (SELECT MAX(phase) from #{table_name});
+      UPDATE #{table_name} SET toggle = true FROM (
+        SELECT prev_phase, the_geom
+        FROM #{table_name} g
+        INNER JOIN (SELECT MAX(phase) AS max_phase FROM #{table_name} g) a ON g.phase = a.max_phase
+      ) a
+      WHERE #{table_name}.phase = a.prev_phase
+        AND #{table_name}.toggle = false
+        AND ST_Intersects(#{table_name}.the_geom, a.the_geom);
+
+      DELETE FROM #{table_name} WHERE phase = (SELECT MAX(phase) FROM #{table_name});
     SQL
   end
 end
