919a6dc4c7a7624db8018e2c34f71565c18cb106
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-07-15 16:49:17 +0100

    support authentication via token in User#find_for_authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,17 @@ class User < ActiveRecord::Base
 
 
   def self.find_for_authentication(warden_conditions)
-    subdomain = warden_conditions[:subdomain].split('.').first
-    country = Country.find_by_subdomain(subdomain)
+    if warden_conditions[:authentication_token]
+      where(authentication_token: warden_conditions[:authentication_token]).first
+    else
+      subdomain = warden_conditions[:subdomain].split('.').first
+      country = Country.find_by_subdomain(subdomain)
 
-    users_with_email = where(:email => warden_conditions[:email])
-    users_with_email.detect { |user|
-      user.super_admin? || user.country == country
-    }
+      users_with_email = where(:email => warden_conditions[:email])
+      users_with_email.detect { |user|
+        user.super_admin? || user.country == country
+      }
+    end
   end
 
   def super_admin?
