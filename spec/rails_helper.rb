ace7715b95a63bfb577087213c572f5b880560db
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-07-20 01:41:49 +0100

    make signed_in? helper method behave in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,9 +13,11 @@ module ControllerHelpers
     if user.nil?
       allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
       allow(controller).to receive(:current_user).and_return(nil)
+      allow(controller).to receive(:signed_in?).and_return(false)
     else
       allow(request.env['warden']).to receive(:authenticate!).and_return(user)
       allow(controller).to receive(:current_user).and_return(user)
+      allow(controller).to receive(:signed_in?).and_return(true)
     end
   end
 end
