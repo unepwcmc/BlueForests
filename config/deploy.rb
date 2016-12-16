e76a0e53a13f685a7386cf6e04c5d4a79d7d19e4
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-12-16 11:20:06 +0000

    bring deploy branch back to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,11 +7,7 @@ set :repo_url, 'git@github.com:unepwcmc/blueforests.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-
-set :branch, 'server-migration'
-
 set :deploy_user, 'wcmc'
-
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
@@ -20,16 +16,12 @@ set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
 set :scm, :git
 set :scm_username, "unepwcmc-read"
 
-
 set :rvm_type, :user
 set :rvm_ruby_version, '2.2.3'
-
-
 
 set :ssh_options, {
   forward_agent: true,
 }
-
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -55,4 +47,3 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :keep_releases, 5
 
 set :passenger_restart_with_touch, false
-
