8d6bb4c3f0b0205e54bd2c9eb148777c3fa41c39
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-01-24 11:49:55 +0000

    save field sites to cartodb on create

--- app/models/area.rb
+++ app/models/area.rb
@@ -7,8 +7,19 @@ class Area < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :coordinates, presence: true
 
+  after_create :upload_to_carto
+
   def geo_json
-    "{\"type\":\"Polygon\",\"coordinates\":#{coordinates}}"
+    %{
+      {
+        "type":"Polygon",
+        "coordinates": [#{coordinates}],
+        "crs":{
+          "type":"name",
+          "properties":{"name":"EPSG:4326"}
+        }
+      }
+    }
   end
 
   def json_coordinates
@@ -26,5 +37,18 @@ class Area < ActiveRecord::Base
     Habitat.all.each do |habitat|
       mbtiles << Mbtile.new(habitat: habitat.name)
     end
+  end
+
+  def upload_to_carto
+    query = """
+      INSERT INTO blueforests_field_sites_#{Rails.env} (the_geom, country_id, name)
+      VALUES (
+        ST_SetSRID(ST_GeomFromGeoJSON(#{ActiveRecord::Base.sanitize(geo_json)}), 4326),
+        #{ActiveRecord::Base.sanitize(country.iso)},
+        #{ActiveRecord::Base.sanitize(title)}
+      )
+    """
+
+    CartoDb.query(query)
   end
 end
