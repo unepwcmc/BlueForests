ea93a2a4684b440c02c9228c4f975eff0deb7e5b
Will Kocur <will.kocur@unep-wcmc.org>
2020-07-29 10:52:41 +0100

    updage country homepage text and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
 
     link_to text, path, opts
   end
+
+  def gef_site_link slug, name
+    "<p>See <a href=\"https://www.gefblueforests.org/project-site-locations/#{slug}-site\" title=\"Go to GEF Blue Forests - #{name}\" target=\"_blank\">here</a> for further details.</p>".html_safe
+  end
 end
